1766abfc81a20a5239369c107ff30abfc010f2f4
alekskrd <als.d@mail.ru>
2022-09-18 22:47:02 +0300

    добавил break

--- array/Program.cs
+++ array/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {2, 5, 100, 3, 8, 3, 7, 4, 100};
+
+int n = array.Length;
+int find = 100;               
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;                      //для того чтобы не показывала два индекса
+    }
+    index++;
+}
