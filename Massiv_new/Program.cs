3d52980dce19d411dd4a189c6213174fd64efea0
alekskrd <als.d@mail.ru>
2022-09-18 23:03:01 +0300

    цикл

---
+++ Massiv_new/Program.cs
@@ -0,0 +1,28 @@
+﻿void FilllArray(int[] collection)
+{
+    int length = collection.Length;
+        int index = 0;
+        while (index < length)
+        {
+            collection[index] = new Random().Next(1, 10);
+            index++;
+        }
+}
+
+void PrintArray(int[] col)    //void ничего не возврашает
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int[] array = new int[10];
+
+FilllArray (array);
+PrintArray(array);
+    
